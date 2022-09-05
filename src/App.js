58d87f361c1b6d62db2f316968259481c9da4c38
md7o <md7ohe@gmail.com>
2022-09-05 23:12:09 +0300

    commit message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Main from "./components/Main";
 import Services from "./components/Services";
 import AOS from "aos";
@@ -8,10 +8,14 @@ import "aos/dist/aos.css";
 function App() {
   AOS.init();
   return (
-    <Router>
-      <Route path="/Services" element={Services} />
-      <Route path="/" exact component={Main} />
-    </Router>
+    <Routes>
+      <Route exact path="/">
+        <Main />
+      </Route>
+      <Route path="/Services">
+        <Services />
+      </Route>
+    </Routes>
   );
 }
 export default App;
